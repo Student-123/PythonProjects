642563762e3ea6c6e3910c206b9cb69d84e017d9
Student <studentnumber123@gmail.com>
2018-01-04 22:55:34 +0530

    Logic to do calculations done

--- ExcelMerger/src/Merger.py
+++ ExcelMerger/src/Merger.py
@@ -13,6 +13,19 @@ class HMBType:
     pregnant = BreedType()
     milkYield = BreedType()
 
+class FullRecord:
+    id = ''
+    age = ''
+    breed = ''
+    status = ''
+    breedingStatus = ''
+    sinceLastAI = ''
+    daysInMilk = ''
+    toConception = ''
+    avgMilk = ''
+    routeName = ''
+    hmbName = ''
+
 print('Loading the configuration')
 configBook = openpyxl.load_workbook(os.path.join('..','Config','Config.xlsx'))
 configSheet = configBook.get_active_sheet()
@@ -64,6 +77,25 @@ def replaceBreedNames(breedCols):
         if not matchedStart:
             breedCell.value = breedMap['default']
 
+def convertFullRowsToObject(fullSheet):
+    fullRows = fullSheet[2:fullSheet.max_row]
+    objectRecords = []
+    for fullRow in fullRows:
+        convertedObject = FullRecord()
+        convertedObject.id = fullRow[0].value
+        convertedObject.age = fullRow[1].value
+        convertedObject.breed = fullRow[2].value
+        convertedObject.status = fullRow[3].value
+        convertedObject.breedingStatus = fullRow[4].value
+        convertedObject.sinceLastAI = fullRow[5].value
+        convertedObject.daysInMilk = fullRow[6].value
+        convertedObject.toConception = fullRow[7].value
+        convertedObject.avgMilk = fullRow[8].value
+        convertedObject.routeName = fullRow[9].value
+        convertedObject.hmbName = fullRow[10].value
+        objectRecords.append(convertedObject)
+    return objectRecords
+
 def mergeFiles():
     print('Files for merging:')
     fileList = os.listdir(os.path.join('..', 'InputForMerge'))
@@ -99,7 +131,64 @@ def doHMBProcessing():
         hmbSheet = hmbWorkbook.get_sheet_by_name('HMB wise')
         breedColumns = outputSheet['I'][1:]
         replaceBreedNames(breedColumns)
+        fullRecords = convertFullRowsToObject(fullWorksheet)
+        hmbIndexedMap = {}
+        hmbConsolidatedMap = {}
+        for fullRecord in fullRecords:
+            if(fullRecord.hmbName in hmbIndexedMap):
+                hmbIndexedMap[fullRecord.hmbName].append(fullRecord)
+            else:
+                indexedList  = []
+                indexedList.append(fullRecord)
+                hmbIndexedMap[fullRecord.hmbName] = indexedList
 
+        for location in hmbIndexedMap.keys():
+            locationRecords = hmbIndexedMap[location]
+            for locRecord in locationRecords:
+                if locRecord.status == 'Milking':
+                    if locRecord.hmbName not in hmbConsolidatedMap:
+                        newHmbType = HMBType()
+                        newHmbType.bred = BreedType()
+                        newHmbType.unbred = BreedType()
+                        newHmbType.pregnant = BreedType()
+                        hmbConsolidatedMap[location] = newHmbType
+                    if locRecord.breedingStatus == 'Open Unbred':
+                        hmbConsolidatedMap[location].unbred.total = (hmbConsolidatedMap[location].unbred.total)+1
+                        if locRecord.status == 'HF':
+                            hmbConsolidatedMap[location].unbred.HF = (hmbConsolidatedMap[
+                                    location].unbred.HF) + 1
+                        elif locRecord.status == 'Jersey':
+                                hmbConsolidatedMap[location].unbred.Jy = (hmbConsolidatedMap[
+                                    location].unbred.Jy) + 1
+                        else:
+                                hmbConsolidatedMap[location].unbred.ND = (hmbConsolidatedMap[
+                                    location].unbred.ND) + 1
+                    elif locRecord.breedingStatus == 'Open Bred':
+                        hmbConsolidatedMap[location].bred.total = (hmbConsolidatedMap[location].bred.total) + 1
+                        if locRecord.status == 'HF':
+                                hmbConsolidatedMap[location].bred.HF = (hmbConsolidatedMap[
+                                    location].bred.HF) + 1
+                        elif locRecord.status == 'Jersey':
+                                hmbConsolidatedMap[location].bred.Jy = (hmbConsolidatedMap[
+                                    location].bred.Jy) + 1
+                        else:
+                                hmbConsolidatedMap[location].bred.ND = (hmbConsolidatedMap[
+                                    location].bred.ND) + 1
+                    elif locRecord.breedingStatus == 'Pregnant':
+                        hmbConsolidatedMap[location].pregnant.total = (hmbConsolidatedMap[location].pregnant.total) + 1
+                        if locRecord.status == 'HF':
+                                hmbConsolidatedMap[location].pregnant.HF = (hmbConsolidatedMap[
+                                    location].pregnant.HF) + 1
+                        elif locRecord.status == 'Jersey':
+                                hmbConsolidatedMap[location].pregnant.Jy = (hmbConsolidatedMap[
+                                    location].pregnant.Jy) + 1
+                        else:
+                                hmbConsolidatedMap[location].pregnant.ND = (hmbConsolidatedMap[
+                                    location].pregnant.ND) + 1
+        hosurData = hmbConsolidatedMap['HOSUR']
+        print(hosurData.unbred.total)
+        print(hosurData.bred.total)
+        print(hosurData.pregnant.total)
 
 print('1: Merging')
 print('2: HMB Processing')
